1d111ff8fa218da0e0744dc23048c14191b11de2
Moritz Fuller <moritz.fuller@dfinity.org>
2024-02-05 17:02:53 +0100

    ✨ added projects

--- main.js
+++ main.js
@@ -1,24 +1,256 @@
 let projects = [
     {
-        url: "projects#sechs-monate-amerika",
-        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba687c6_archive_image_101.jpg",
+        url: "projects#item-magazin-4",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3adf5c3943a3701d22f_Landing-Page_1.jpg",
         desktop:
-            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba687c6_archive_image_101.jpg",
-        description: "Sechs Monate Amerika",
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba6871c_item-magazin-4_1.jpg",
+        description:
+            "item Magazin #4, annual Lectureship at HTW Berlin, Department of Design",
     },
     {
-        url: "projects#am-abgrund-der-bilder",
-        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba687c5_archive_image_103.jpg",
+        url: "projects#der-geschenkte-tag",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3ac4fa7ce0cb3489596_Landing-Page_2.jpg",
         desktop:
-            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba687c5_archive_image_103.jpg",
-        description: "Am Abgrund der Bilder",
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba68653_der-geschenkte-tag_2.jpg",
+        description:
+            "Catalogue Raisonné for Michael Müller on the occasion of his solo exhibition at the Städel Museum Frankfurt",
+    },
+    {
+        url: "projects#deutsches-historisches-museum",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3ac3db97df5c9321152_Landing-Page_3.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba68666_deutsches-historisches-museum_4.jpg",
+        description:
+            "Deutsches Historisches Museum Berlin, printed matter for various exhibitions",
     },
     {
         url: "projects#kunstsaele",
-        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba687c4_archive_image_104.jpg",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3ac5b2f5e93f14a5822_Landing-Page_4.jpg",
         desktop:
-            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba687c4_archive_image_104.jpg",
-        description: "Kunstsaele",
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba6870f_kunstsaele_1.jpg",
+        description:
+            "956-pages Catalog for the 10-year-anniversary of Kunstsӕle Berlin",
+    },
+    {
+        url: "projects#ministerium-kultur-nrw",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3ad244539e22313b650_Landing-Page_5.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba6873b_Ministerium-fur-kultur_2.jpg",
+        description:
+            "Magazine Design for the Ministerium für Kultur und Wissenschaft NRW",
+    },
+    {
+        url: "projects#am-abgrund-der-bilder",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3acd9ff38f7048ef941_Landing-Page_6.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba686ef_am-abgrund-der-bilder_1.jpg",
+        description:
+            "Catalog for Michael Müller’s exhibition at the Museum im Kulturspeicher Würzburg",
+    },
+    {
+        url: "projects#item-magazin-3",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3ad5a57b4b1f3e55876_Landing-Page_7.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d39b358c5fe6bbb6ffe0_Landing-Page_7.jpg",
+        description:
+            "item Magazin #3, annual Lectureship at HTW Berlin, Department of Design",
+    },
+    {
+        url: "projects#studio-michael-mueller",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3acb891b647e8b125aa_Landing-Page_8.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba686e5_studio-michael-mueller_1.jpg",
+        description: "Visual identity and web design for Studio Michael Müller",
+    },
+    {
+        url: "projects#deutsches-historisches-museum",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3ad7537e362731f036f_Landing-Page_9.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d39c43da67dca6b8fc2a_Landing-Page_9.jpg",
+        description:
+            "Deutsches Historisches Museum Berlin, printed matter for various exhibitions",
+    },
+    {
+        url: "projects#studio-visit",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3ad797a98a093932569_Landing-Page_10.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba6872a_studio-visit_1.jpg",
+        description: "Publication for Studio Michael Müller",
+    },
+    {
+        url: "projects#item-magazin-1",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3ada884445bf832cbfe_Landing-Page_11.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d39bc964e153bd1b1b90_Landing-Page_11.jpg",
+        description:
+            "item Magazin #1, annual Lectureship at HTW Berlin, Department of Design",
+    },
+    {
+        url: "projects#am-abgrund-der-bilder",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3ac09a951ab0c57bf7f_Landing-Page_12.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba686ec_am-abgrund-der-bilder_3.jpg",
+        description:
+            "Catalog for Michael Müller’s exhibition at the Museum im Kulturspeicher Würzburg",
+    },
+    {
+        url: "projects#waterslide",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3ad955dcc74fb98c93e_Landing-Page_13.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba68750_waterslide_2.jpg",
+        description: "Visual identity and web design for waterslide",
+    },
+    {
+        url: "projects#cozy-gang",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3adb891b647e8b125d3_Landing-Page_14.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d39bc964e153bd1b1ba3_Landing-Page_14.jpg",
+        description: "Textile design for Cozy Gang",
+    },
+    {
+        url: "projects#der-geschenkte-tag",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3ad7505cd1896e2e1a0_Landing-Page_15.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba68657_der-geschenkte-tag_1.jpg",
+        description:
+            "Catalogue Raisonné for Michael Müller on the occasion of his solo exhibition at the Städel Museum Frankfurt",
+    },
+    {
+        url: "projects#deutsches-historisches-museum",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3ac2fa7c492cc20d89f_Landing-Page_16.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba6865f_deutsches-historisches-museum_1.jpg",
+        description:
+            "Deutsches Historisches Museum Berlin, printed matter for various exhibitions",
+    },
+    {
+        url: "projects#sechs-monate-amerika",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3ac91312d16cec4e34d_Landing-Page_17.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d39bdf84865a88e644ca_Landing-Page_17.jpg",
+        description:
+            "Photo Book with over 800 images from North and South America",
+    },
+    {
+        url: "projects#neununddreissig-null",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3ad7505cd1896e2e16c_Landing-Page_18.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba6879a_neununddreissig-null_1.jpg",
+        description: "Redesign for 39NULL Magazine",
+    },
+    {
+        url: "projects#fpdao",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3acff8b4b6be4bc6fef_Landing-Page_19.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba686fb_fpdao_4.jpg",
+        description: "Visual Identity and web design for FP-DAO",
+    },
+    {
+        url: "projects#item-magazin-1",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3ad85c801eea2a42c28_Landing-Page_20.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba68788_item-magazin-1_6.jpg",
+        description:
+            "item Magazin #1, annual Lectureship at HTW Berlin, Department of Design",
+    },
+    {
+        url: "projects#embroidery",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3adfaadb6c8a5c7893e_Landing-Page_21.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba6877a_embroidery_1.jpg",
+        description: "Embroidered typography on textile",
+    },
+    {
+        url: "projects#item-magazin-3",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3ad6c9cafa965e5bc42_Landing-Page_22.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba68741_item-magazin-3_3.jpg",
+        description:
+            "item Magazin #3, annual Lectureship at HTW Berlin, Department of Design",
+    },
+    {
+        url: "projects#ministerium-kultur-nrw",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3adc964e153bd1b27e7_Landing-Page_23.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba68736_Ministerium-fur-kultur_1.jpg",
+        description:
+            "Magazine Design for the Ministerium für Kultur und Wissenschaft NRW",
+    },
+    {
+        url: "projects#jenny-gleitsmann",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3acd6711a0b2d62511b_Landing-Page_24.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba68758_jenny-gleitsmann_1.jpg",
+        description: "Visual identity and web design for Jenny Gleitsmann",
+    },
+    {
+        url: "projects#item-magazin-4",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3ad220a4806fbfbf3d1_Landing-Page_25.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d39cdf84865a88e64519_Landing-Page_25.jpg",
+        description:
+            "item Magazin #4, annual Lectureship at HTW Berlin, Department of Design",
+    },
+    {
+        url: "projects#studio-visit",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3ada884445bf832cba7_Landing-Page_26.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba68720_studio-visit_3.jpg",
+        description: "Publication for Studio Michael Müller",
+    },
+    {
+        url: "projects#operator",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3ac9e1688e2b533bba5_Landing-Page_27.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba6872e_operator_2.jpg",
+        description:
+            "Visual Identity for Operator’s NFT-Series “Privacy Collection“ by the artists Ania Catherine and Dejha Ti",
+    },
+    {
+        url: "projects#studio-michael-mueller",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3adb7715c09a62af0aa_Landing-Page_28.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba686e9_studio-michael-mueller_6.jpg",
+        description: "Visual identity and web design for Studio Michael Müller",
+    },
+    {
+        url: "projects#xyz-festival",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3ad40da098b1e458a64_Landing-Page_29.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba6877f_xyz-festival_1.jpg",
+        description:
+            "Visual Identity for XYZ Design Festival, showcased at BAM Berlin",
+    },
+    {
+        url: "projects#der-geschenkte-tag",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3ae3e39ff0ecc042822_Landing-Page_30.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba6865c_der-geschenkte-tag_4.jpg",
+        description:
+            "Catalogue Raisonné for Michael Müller on the occasion of his solo exhibition at the Städel Museum Frankfurt",
+    },
+    {
+        url: "projects#ministerium-kultur-nrw",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3ac4f860782fe2a3edf_Landing-Page_31.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba6873c_Ministerium-fur-kultur_5.jpg",
+        description:
+            "Magazine Design for the Ministerium für Kultur und Wissenschaft NRW",
+    },
+    {
+        url: "projects#sechs-monate-amerika",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3ad1f8f89231f57ec75_Landing-Page_32.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba6876b_sech-monate-amerika_1.1.jpg",
+        description:
+            "Photo Book with over 800 images from North and South America",
+    },
+    {
+        url: "projects#waterslide",
+        mobile: "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65c0d3ad04c45b03cec2e090_Landing-Page_33.jpg",
+        desktop:
+            "https://uploads-ssl.webflow.com/65bcc9611f29c99b6ba68602/65bcc9611f29c99b6ba6874e_waterslide_1.jpg",
+        description: "Visual identity and web design for waterslide",
     },
 ];
 
